f73d7897d9f26c058f338e181c1c909194231782
Henry Do <661034+vitaminh@users.noreply.github.com>
2018-11-19 11:36:03 -0800

    longest common prefix

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -1,3 +1,12 @@
-const longestCommonPrefix = () => {};
+const longestCommonPrefix = strs => {
+  if (!strs.length) return '';
+  for (let charIdx = 0; charIdx < strs[0].length; charIdx++) {
+    console.log('str[i].length: ', strs[0].length);
+    for (let strIdx = 0; strIdx < strs.length; strIdx++) {
+      if (strs[strIdx][charIdx] !== strs[0][charIdx]) return strs[0].slice(0, charIdx);
+    }
+  }
+  return strs[0];
+};
 
 module.exports = longestCommonPrefix;
--- longestCommonPrefix.spec.js
+++ longestCommonPrefix.spec.js
@@ -1,17 +1,23 @@
 const { expect } = require('chai');
 const longestCommonPrefix = require('./longestCommonPrefix');
 
-describe('wrap', () => {
-  it('Returns empty string if empty string was provided', () => {
-    expect(wrap('', 10)).to.equal('');
+describe('longestCommonPrefix', () => {
+  it('Returns empty string if empty array was provided', () => {
+    expect(longestCommonPrefix([])).to.equal('');
   });
-  it('Returns one line', () => {
-    expect(wrap('the', 2)).to.equal('th\ne');
+  it('Returns one letter if one letter matches', () => {
+    expect(longestCommonPrefix(['foo', 'faa', 'fee'])).to.equal('f');
   });
-  it('Returns two line', () => {
-    expect(wrap('the string', 5)).to.equal('the\nstrin\ng');
+  it('Returns entire word if entire word matches', () => {
+    expect(longestCommonPrefix(['foo', 'foo', 'foo'])).to.equal('foo');
   });
-  it('Returns all spaces in new line', () => {
-    expect(wrap('the new string in', Infinity)).to.equal('the new string in');
+  it('Returns nothing if nothing matches', () => {
+    expect(longestCommonPrefix(['abc', 'def', 'ghi', 'jkl'])).to.equal('');
+  });
+  it('Returns numbers if numbers matche', () => {
+    expect(longestCommonPrefix(['123', '134', '1', '176'])).to.equal('1');
+  });
+  it('Returns numbers if numbers matche', () => {
+    expect(longestCommonPrefix([1234, 134, 1, 176])).to.equal(1);
   });
 });
