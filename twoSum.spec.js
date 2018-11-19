c29778eeb78cfe890cd5c8376932d0fd0534d7fd
Victor Marta <vicm159@gmail.com>
2018-11-19 10:55:04 -0800

    twoSum solution

--- twoSum.spec.js
+++ twoSum.spec.js
@@ -3,18 +3,18 @@ const twoSum = require('./twoSum');
 
 describe('twoSum', () => {
   it('Returns empty array when empty array is passed', () => {
-    expect(twoSum([], 10)).to.equal([]);
+    expect(twoSum([], 10)).to.eql([]);
   });
   it('Returns proper indices when sum is found', () => {
-    expect(twoSum([3, 5, 10], 15)).to.equal([1, 2]);
+    expect(twoSum([3, 5, 10], 15)).to.eql([1, 2]);
   });
   it('Returns empty array when sum is not found', () => {
-    expect(twoSum([1, 2, 3, 4, 5], 100)).to.equal([]);
+    expect(twoSum([1, 2, 3, 4, 5], 100)).to.eql([]);
   });
   it('Returns empty array when sum is not found', () => {
-    expect(twoSum([1, 2, 3, 4, 100], 100)).to.equal([]);
+    expect(twoSum([1, 2, 3, 4, 100], 100)).to.eql([]);
   });
   it('Returns first pair that sum up to target value', () => {
-    expect(twoSum([50, 50, 50, 50, 50], 100)).to.equal([0, 1]);
+    expect(twoSum([50, 50, 50, 50, 50], 100)).to.eql([0, 1]);
   });
 });
